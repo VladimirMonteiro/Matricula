9a7d5437025dfd1b096912f8f1fa326f097d93fe
vladimirmonteiro <vladimir.monteiro021@gmail.com>
2024-03-26 22:29:13 -0300

    config: resolve cors

--- index.js
+++ index.js
@@ -7,19 +7,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(express.json())
-app.use((req, res, next)=> {
-
-
-    res.header('Access-Control-Allow-Origin', '*'); // ou especificar um domínio permitido
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-
-
-
-
 app.use(cors())
-next()
-})
 
 
 
