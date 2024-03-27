091e8914f192345973ca2aa16c134fdd6f0bef19
vladimirmonteiro <vladimir.monteiro021@gmail.com>
2024-03-26 23:00:05 -0300

    config: resolve cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://trabalho-six-pink.vercel.app/');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
