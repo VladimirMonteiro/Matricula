da1336baf5eda4fb7f1dac78d4a98390f169f291
vladimirmonteiro <vladimir.monteiro021@gmail.com>
2024-03-26 22:51:29 -0300

    config: resolve cors

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(express.json())
-app.use(cors())
-
+app.use(cors({
+    origin: 'https://trabalho-six-pink.vercel.app/'
+  }));
 
 
 // Connection in the DB
