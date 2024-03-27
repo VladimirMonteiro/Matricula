8e63540029cff18057964cedcbe8c0d983412f9d
vladimirmonteiro <vladimir.monteiro021@gmail.com>
2024-03-27 09:53:09 -0300

    config: resolve cors

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const app = express()
 const port = process.env.PORT || 5000
 
 
-app.use(cors({credentials: true, origin: '*'}))
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 
 app.use(express.json())
 
