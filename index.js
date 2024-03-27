e1b9c82c3973517c84d6140d187986d77685e721
vladimirmonteiro <vladimir.monteiro021@gmail.com>
2024-03-27 10:34:24 -0300

    config: resolve cors

--- index.js
+++ index.js
@@ -7,14 +7,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-    });
-
+app.use(cors())
 app.use(express.json())
 
 // Connection in the DB
