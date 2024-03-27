dc734ecdbbbeedc84e0336d8dc06ffc8e126479b
vladimirmonteiro <vladimir.monteiro021@gmail.com>
2024-03-26 22:57:52 -0300

    config: resolve cors

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(express.json())
-app.use(cors({
-    origin: 'https://trabalho-six-pink.vercel.app/'
-  }));
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://trabalho-six-pink.vercel.app/');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
 
 // Connection in the DB
 require('./db/conn')
