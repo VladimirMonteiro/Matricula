ea60712976e959f7bdd2498bca2aa142253ac3ae
vladimirmonteiro <vladimir.monteiro021@gmail.com>
2024-03-28 13:42:59 -0300

    feat: added turma and course

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = 5000
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Adicionando 'Authorization' aqui
 
     app.use(cors())
     next();
